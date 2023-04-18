1fb12ba7efaac277b8b10d7a41be96922b730d4e
tranchau120103 <125194193+tranchau120103@users.noreply.github.com>
2023-04-18 17:19:07 +0700

    Update bai2.cpp

--- bai2.cpp
+++ bai2.cpp
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
 
-int  tìm kiếm tuyến tính ( int a[], int n, int x){
-	cho ( int i= 0 ; i<n; i++){
-		nếu (a[i] == x){
-			trả lại tôi;
+int linearsearch(int a[], int n, int x){
+	for(int i=0; i<n; i++){
+		if(a[i] == x){
+			return i;
 		}
-	} trả về - 1 ;
+	}return -1;
 }
 
-int  chính (){
-	int a[] = { 2 , 3 , 4 , 10 , 40 };
-	int n = 5 ;
-	int x = 10 ;
-	int ketqua = linearsearch (a,n,x);
-	nếu (kết quả == - 1 ){
-		printf ( " Không thời gian thay phần tử " );
-	} khác
-		printf ( " Phan tu %do vi tri a[%d] " , x, ketqua);
-	trả về  0 ;
+int main(){
+	int a[] = {2, 3, 4, 10, 40};
+	int n = 5;
+	int x = 10;
+	int ketqua = linearsearch(a, n, x);
+	if(ketqua == -1){
+		printf("Khong tim thay phan tu");
+	}else
+		printf("Phan tu %d o vi tri a[%d]", x, ketqua);
+	return 0;
 }
