3682813ff1b6c9d3c93aa73eb519e8d6bbb5ea44
tranchau120103 <125194193+tranchau120103@users.noreply.github.com>
2023-04-18 17:18:23 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <conio.h>
 
-int  tìm kiếm nhị phân ( int a[], int n, int x){
-	int l = 0 , r = n- 1 ;
-	trong khi (l < r){
-		int mid = (l+r)/ 2 ;
-		nếu (a[giữa] == x){
-			trở về giữa;
+int binarysearch(int a[], int n, int x){
+	int l = 0, r = n-1;
+	while(l < r){
+		int mid = (l+r)/2;
+		if(a[mid] == x){
+			return mid;
 		}
-		khác  nếu (a[giữa] < x){
-			l = giữa + 1 ;
+		else if(a[mid] < x){
+			l = mid + 1;
 		}
-		khác 
-			r = giữa;
+		else 
+			r = mid;
 	}
-	trả về - 1 ;
+	return -1;
 }
 
-int  chính (){
-	int a[] = { 2 , 3 , 4 , 10 , 40 };
-	int n = 5 ;
-	int x = 10 ;
-	int ketqua = binarysearch (a,n,x);
-	nếu (kết quả == - 1 ){
-		printf ( " khong tim thay phan tu " );
-	} khác
-		printf ( " Phan tu %do vi tri a[%d] " , x, ketqua);
+int main(){
+	int a[] = {2, 3, 4, 10, 40};
+	int n = 5;
+	int x = 10;
+	int ketqua = binarysearch(a, n, x);
+	if(ketqua == -1){
+		printf("khong tim thay phan tu");
+	}else
+		printf("Phan tu %d o vi tri a[%d]", x, ketqua);
 }
